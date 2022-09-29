06e58778c6d249156724d4b46f7eb810e397dc5d
Thanhlongcs <thanhlongcf010@gmail.com>
2022-09-29 14:27:43 +0700

    up

---
+++ src/main/java/rikkei/academy/demotom3/HelloServlet.java
@@ -0,0 +1,34 @@
+package rikkei.academy.demotom3;
+
+import java.io.*;
+import java.util.Date;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "ServerTimeServlet", value = "/ServerTimeServlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h1>" + message +"</h1>");
+        writer.println("</body></html>");
+        writer.println("<html>");
+
+        Date today = new Date();
+        writer.println("<h1>" + today +"</h1>");
+        writer.println("</html>");
+
+    }
+
+    public void destroy() {
+    }
+}
